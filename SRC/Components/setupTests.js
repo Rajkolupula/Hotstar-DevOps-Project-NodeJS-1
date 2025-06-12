6f02697761130139e23d0152296712430a7ec6eb
Kolupula Raju <rkolupula589@gmail.com>
2025-06-12 12:43:21 +0530

    Add files via upload

---
+++ SRC/Components/setupTests.js
@@ -0,0 +1,6 @@
+// src/setupTests.js
+
+// Adds custom Jest matchers for asserting on DOM nodes.
+// Allows you to do things like:
+// expect(element).toBeInTheDocument()
+import '@testing-library/jest-dom';
